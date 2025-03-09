789fe53fe7bd2923bce323369ccab4d06152d8fc
pcortellezzi <philippe.cortellezzi@gmail.com>
2025-03-09 17:35:48 -0300

    Add auto build proto files

--- build.rs
+++ build.rs
@@ -1,6 +1,22 @@
-fn main() {
-    // let config = prost_build::Config::new()
-    //     .out_dir("src")
-    //     .compile_protos(&["src/raw-proto/otps_proto_pool.proto"], &["src/raw-proto"])
-    //     .unwrap();
+use std::fs;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let mut proto_files = vec![];
+
+    for entry in fs::read_dir("src/raw-proto/").unwrap() {
+        let entry_path = entry.unwrap().path();
+        if entry_path.is_file() {
+            if let Some(extension) = entry_path.extension() {
+                if extension == "proto" {
+                    proto_files.push(entry_path.display().to_string())
+                }
+            }
+        }
+    }
+
+    let mut config = prost_build::Config::default();
+
+    config.compile_protos(&proto_files, &["./src/raw-proto/"])?;
+    Ok(())
 }
